6e57111d93b196b174fe8bacf74cfade2dd5387e
Gabriel Cabral de Santana Ribeiro <166030912+GabrielCSR-dev@users.noreply.github.com>
2025-05-27 20:29:54 -0300

    Add files via upload

---
+++ controlegastos/controlegastos/src/test/java/br/edu/ifba/controlegastos/ControlegastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.controlegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlegastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
