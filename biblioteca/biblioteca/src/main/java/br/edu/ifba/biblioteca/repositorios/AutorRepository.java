bf9e9b3c2b0175ef7595a2070faff4e3b2f03354
Gabriel Cabral de Santana Ribeiro <166030912+GabrielCSR-dev@users.noreply.github.com>
2025-05-28 21:06:47 -0300

    Delete biblioteca directory

--- biblioteca/biblioteca/src/main/java/br/edu/ifba/biblioteca/repositorios/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifba.biblioteca.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifba.biblioteca.entidades.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Long>{
-
-}
