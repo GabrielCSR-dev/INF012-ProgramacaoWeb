dba471b93d0b4964fc5a1fe45ff89a7fa0031628
Gabriel Cabral de Santana Ribeiro <166030912+GabrielCSR-dev@users.noreply.github.com>
2025-06-05 21:56:22 -0300

    Add files via upload

---
+++ segurancaApp/segurancaApp/src/main/java/br/edu/ifba/segurancaApp/servicos/AutenticacaoService.java
@@ -0,0 +1,24 @@
+package br.edu.ifba.segurancaApp.servicos;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifba.segurancaApp.repositorios.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService{
+
+	private UsuarioRepository usuarioRepository;
+	
+	public AutenticacaoService(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return this.usuarioRepository.findByLogin(username);
+	}
+
+}
